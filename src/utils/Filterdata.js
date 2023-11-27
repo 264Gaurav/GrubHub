64470917d03c2180c3f9f90c066cbc6bf0d53e44
GAURAV SINGH <gauravsingh264209@gmail.com>
2023-11-27 16:48:07 +0530

    Delete src directory

--- src/utils/Filterdata.js
+++
@@ -1,8 +0,0 @@
-export const filterRestaurant = (searchText, allRestaurants) => {
-    const filteredData = allRestaurants.filter((restaurant) => {
-      return (
-        restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())
-      );
-    });
-    return filteredData;
-};
